9ae5a58bdb1f9c9a8892bd6c0d8411daca02e59a
kazuki minemura <kazuki.minemura@intel.com>
2022-02-17 17:08:39 +0800

    added the case of no object detected

--- demos/smartlab_demo/python/object_detection/detector.py
+++ demos/smartlab_demo/python/object_detection/detector.py
@@ -149,11 +149,13 @@ class Detector:
 
             if i == 0:
                 all_preds = np.array(preds)
-            elif len(all_preds) == 1:
+            elif len(all_preds) == 0:
                 all_preds = np.zeros((1, 7))
             else:
                 np.vstack((all_preds, preds))
 
+        if len(all_preds) == 0:
+            all_preds = np.zeros((1, 7))
         # merge same classes from model 2
         for r, pred in enumerate(all_preds):
             cls_id = int(pred[-1])
